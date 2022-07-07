a6daadef387c2bd5cde958946e82e3c619def4bb
Gustavo <87251711+CodeWalker3@users.noreply.github.com>
2022-07-07 09:58:35 -0300

    Add files via upload

--- server.py
+++ server.py
@@ -8,6 +8,20 @@ def get_users():
     users = db.query_db('select * from users')
     return jsonify(users),200
 
+@app.route("/pedidos",methods=['POST'])
+def add_pedidos():
+    if request.is_json:
+        pedido = request.get_json()
+        id = db.insert_pedidos((pedido['nome'],pedido['status_pedido'], pedido['data_de_criacao'],pedido['data_de_atualizacao'], pedido['rua'], pedido['numero'], pedido['bairro']))
+#        (nome, status_pedido, data_de_criacao, data_de_atualizacao,rua,numero,bairro)
+        return {"id":id}, 201
+    return {"error": "Request must be JSON"}, 415
+
+@app.route("/pedidos",methods=['GET'])
+def get_pedidos():
+    pedidos = db.query_db('select * from pedidos')
+    return jsonify(pedidos),200
+
 @app.route("/users",methods=['POST'])
 def add_user():
     if request.is_json:
@@ -15,9 +29,8 @@ def add_user():
         id = db.insert((user['nome'],user['email']))
         return {"id":id}, 201
     return {"error": "Request must be JSON"}, 415
-
 if __name__ == '__main__':
-    init_db = True
+    init_db = False
     
     db.init_app(app)
     
